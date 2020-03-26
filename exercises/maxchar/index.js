a1886d8b810e701e75d2173c2a76329b3bb09413
Carl Ortutay <carl.orty@gmail.com>
2020-03-26 15:07:22 -0400

    maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -1,10 +1,33 @@
-// --- Directions
-// Given a string, return the character that is most
-// commonly used in the string.
-// --- Examples
-// maxChar("abcccccccd") === "c"
-// maxChar("apple 1231111") === "1"
+//* --- Directions
+//* Given a string, return the character that is most
+//* commonly used in the string.
+//* --- Examples
+//* maxChar("abcccccccd") === "c"
+//* maxChar("apple 1231111") === "1"
+//? Similar Questions
+//? What is the most common character in the string?
+//? Does string A hae the same characters as string B (is it an anagram)?
+//? Does the given string have any repeated characters in it?
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
