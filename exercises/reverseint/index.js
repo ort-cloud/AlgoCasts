bb65889e9c72b5792e680fdcc8d653085d8bb28a
Carl Ortutay <carl.orty@gmail.com>
2020-03-26 14:18:30 -0400

    Integer Reversal

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -1,13 +1,50 @@
-// --- Directions
-// Given an integer, return an integer that is the reverse
-// ordering of numbers.
-// --- Examples
-//   reverseInt(15) === 51
-//   reverseInt(981) === 189
-//   reverseInt(500) === 5
-//   reverseInt(-15) === -51
-//   reverseInt(-90) === -9
+//* --- Directions
+//* Given an integer, return an integer that is the reverse
+//* ordering of numbers.
+//* --- Examples
+//*   reverseInt(15) === 51
+//*   reverseInt(981) === 189
+//*   reverseInt(500) === 5
+//*   reverseInt(-15) === -51
+//*   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+//! Solution 1. W/ if statment.
+/* function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  if (n < 0) {
+    return parseInt(reversed) * -1;
+  }
+
+  return parseInt(reversed);
+} */
+
+//! Solution 2. Using Math.sign instead of if statment.
+/* function reverseInt(n) {
+  const reversed = n
+    .toString()
+    .split("")
+    .reverse()
+    .join("");
+
+  return parseInt(reversed) * Math.sign(n);
+} */
+
+//! Solution 3. Same as above. Single statement instead of two.
+function reverseInt(n) {
+  return (
+    parseInt(
+      n
+        .toString()
+        .split("")
+        .reverse()
+        .join("")
+    ) * Math.sign(n)
+  );
+}
 
 module.exports = reverseInt;
